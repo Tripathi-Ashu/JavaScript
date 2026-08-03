55d5ac79a6303d7d70f563997d3e4d190f764a16
Ashutosh tripathi <151380927+Tripathi-Ashu@users.noreply.github.com>
2026-08-03 12:51:51 +0000

    forEach is started

---
+++ Lecture_12/Map_Filter_Reduce.js
@@ -0,0 +1,15 @@
+// forEach , filter , reducer , map , set 
+
+const arr = [10 , 20 , 30 , 40 , 50 , "Ashutosh"];
+arr.forEach((number , index) =>  {
+    console.log(number , index);
+});
+
+// first is medotri , second or third is optional 
+
+arr.forEach((number) => {
+    sum += number;
+});
+
+console.log(sum);
+
